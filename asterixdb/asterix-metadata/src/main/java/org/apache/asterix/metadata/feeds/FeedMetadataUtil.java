c3c419f6f874f27e30c112327fe9f8ebeb0c513d
Xikui Wang <xkkwww@gmail.com>
2017-02-28 13:41:52 -0800

    fix test cases

--- asterixdb/asterix-metadata/src/main/java/org/apache/asterix/metadata/feeds/FeedMetadataUtil.java
+++ asterixdb/asterix-metadata/src/main/java/org/apache/asterix/metadata/feeds/FeedMetadataUtil.java
@@ -155,7 +155,7 @@ public class FeedMetadataUtil {
                 }
             }
         } catch (Exception e) {
-            throw new AsterixException("Invalid feed parameters", e);
+            throw new AsterixException("Invalid feed parameters. Exception Message:" + e.getMessage() , e);
         }
     }
 
