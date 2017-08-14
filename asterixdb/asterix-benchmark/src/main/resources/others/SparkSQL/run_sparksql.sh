b86c30209a87dab6e01f6283750de009d378c4db
Xikui Wang <xkkwww@gmail.com>
2017-08-14 14:04:21 -0700

    add fix from github

--- asterixdb/asterix-benchmark/src/main/resources/others/SparkSQL/run_sparksql.sh
+++ asterixdb/asterix-benchmark/src/main/resources/others/SparkSQL/run_sparksql.sh
@@ -46,14 +46,14 @@ then
 fi
 
 # Load data
-ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/../../benchmarks/tpch/gen/gen.yml
+HOME=/root ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/../../benchmarks/tpch/gen/gen.yml
 # Configure HDFS
-ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/install_hdfs.yml
-ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/start_hdfs.yml
+HOME=/root ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/install_hdfs.yml
+HOME=/root ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/start_hdfs.yml
 # Configure Spark
-ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/install_spark.yml
-ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/start_spark.yml
-ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/load_tpch.yml
+HOME=/root ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/install_spark.yml
+HOME=/root ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/start_spark.yml
+HOME=/root ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/load_tpch.yml
 # Execute queries
-ansible-playbook  -vvvv -i $INVENTORY --extra-vars="metric='${SYSTEM_NAME}'" $SCRIPT_PATH/ansible/prepare_queries.yml
-ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/execute_queries.yml
+HOME=/root ansible-playbook  -vvvv -i $INVENTORY --extra-vars="metric='${SYSTEM_NAME}'" $SCRIPT_PATH/ansible/prepare_queries.yml
+HOME=/root ansible-playbook  -vvvv -i $INVENTORY $SCRIPT_PATH/ansible/execute_queries.yml
