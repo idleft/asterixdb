c01165564553d6b277bc50057fc8e3e434e1ee0c
Xikui Wang <xkkwww@gmail.com>
2017-08-18 15:09:35 -0700

    remove unneeded import

--- asterixdb/asterix-app/src/main/java/org/apache/asterix/api/http/server/ResultUtil.java
+++ asterixdb/asterix-app/src/main/java/org/apache/asterix/api/http/server/ResultUtil.java
@@ -27,7 +27,6 @@ import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
